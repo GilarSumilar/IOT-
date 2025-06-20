98fe91c3bedab00c5c8ea1de1290d6b12bfc51c2
GilarSumilar <gilarz2100@gmail.com>
2025-06-20 09:39:48 +0700

    add

---
+++ main.js
@@ -0,0 +1,20 @@
+function SetDapurLed() {
+    ledDapur.style.backgroundColor = "red";
+    dapurLedImage.src = "./assets/led-on.png";
+    alert("OK Dapur");
+}
+function SetTamuLed() {
+    ledTamu.style.backgroundColor = "red";
+    tamuLedImage.src = "./assets/led-on.png";
+    alert("OK Tamu");
+}
+function SetMakanLed() {
+    ledMakan.style.backgroundColor = "red";
+    makanLedImage.src = "./assets/led-on.png";
+    alert("OK Makan");
+}
+function SetToiletLed() {
+    ledToilet.style.backgroundColor = "red";
+    toiletLedImage.src = "./assets/led-on.png";
+    alert("OK Toilet");
+}
